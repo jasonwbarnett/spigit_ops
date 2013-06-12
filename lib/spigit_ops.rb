c74cbf620268ce9b0886273496b60a6e06016f52
Jason Barnett <jason.w.barnett@gmail.com>
2013-06-12 12:58:20 -0700

    updated some stuff

--- lib/spigit_ops.rb
+++ lib/spigit_ops.rb
@@ -5,4 +5,7 @@ require "spigit_ops/spigit_conf"
 module SpigitOps
 	TCS_START = 0
 	TCS_END   = 1
+	
+	TC_SERVER_XML    = '/opt/tomcat/conf/server.xml'
+	TC_SERVICES_FILE = '/spigit/data_warehouse/tomcat/services.txt'
 end
--- lib/spigit_ops/tomcat.rb
+++ lib/spigit_ops/tomcat.rb
@@ -2,8 +2,8 @@ require 'nokogiri'
 
 module SpigitOps
   class Tomcat
-    def initialize(serverxml_filename = "/opt/tomcat/conf/server.xml", options= {})
-      attr_reader :serverxml_filename
+    def initialize(serverxml_filename = SpigitOps::TC_SERVER_XML, options= {})
+      @serverxml_filename = serverxml_filename
 
       if ! FileTest.exist? @serverxml_filename
   			$stderr.puts "#{@serverxml_filename} doesn't exist, exiting..."
@@ -13,22 +13,52 @@ module SpigitOps
   			exit
 	  	end
 
-	  	@serverxml_filename = serverxml_filename
 	  	@serverxml_xml = Nokogiri::XML(File.open(@serverxml_filename))
+      build_tomcat_services
 	  end
 
 	  def services
 	  	puts @tomcat_services
 	  end
 
+    def service(name)
+      puts @tomcat_services[name]
+    end
+
+    def serverxml_filename
+      puts @serverxml_filename
+    end
+
+    def save!(output_filename = SpigitOps::TC_SERVICES_FILE)
+      File.open(output_filename, 'w') { |f| @tomcat_services.each { |key, value| f.puts value } }
+    end
+
 	  private
 
-    	def build_services
-        @services = {}
+      ## Create a table so we can lookup the line number for the </Service> for the particular service we want
+      def build_service_end_line_lookup
+        lines = File.open(@serverxml_filename).readlines.collect { |x| x.strip }
+        lines.unshift('placing item in array so the index reflects the actual line number')
+
+        services_start = (0 .. lines.count - 1).find_all { |x| lines[x,1].to_s.match(/<Service/) }
+        services_end   = (0 .. lines.count - 1).find_all { |x| lines[x,1].to_s.match(/<\/Service/) }
+        service_line_numbers = services_start.zip(services_end)
+
+        @service_end_line_lookup = service_line_numbers.inject({}) do |result, element|
+          key   = element.first
+          value = element.last
+          result[key] = value
+          result
+        end
+      end
+
+    	def build_tomcat_services
+        build_service_end_line_lookup
+        @tomcat_services = {}
     		@serverxml_xml.xpath('//Service').each do |service|
     		  name = service[:name]
     		  start_line = service.line
-    		  end_line   = end_line_lookup[start_line]
+    		  end_line   = @service_end_line_lookup[start_line]
 
     		  # Create an array with all connectors and the needed attributes in a hash
     		  connectors = []
@@ -62,10 +92,11 @@ module SpigitOps
     		    end
     		  end
 
-
    		    serviceDefintion = {:start_line => start_line, :end_line => end_line, :name => name, :connectors => connectors.uniq, :docBases => docBase.uniq, :resources => resources}
-   		    @tomcat_services[:name] = serviceDefintion
+   		    @tomcat_services[name] = serviceDefintion
     		end
+
+        @tomcat_services
     	end
 	end
 end
